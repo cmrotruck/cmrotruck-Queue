9ea7a893a281ed4975ff2ab76e3b96f4a0df5acd
Chad Rotruck <crotruck@gmail.com>
2023-02-09 00:43:44 +0000

    Done

--- src/main/java/QueueExercise.java
+++ src/main/java/QueueExercise.java
@@ -1,4 +1,5 @@
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.ArrayDeque;
 
@@ -37,7 +38,8 @@ public class QueueExercise {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        Deque<String> myDeque = new LinkedList<>();
+        return myDeque;
     }
 
     /**
@@ -46,7 +48,7 @@ public class QueueExercise {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        return queue.size();
     }
 
     /**
@@ -56,7 +58,7 @@ public class QueueExercise {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
-
+        queue.add(item);
     }
 
     /**
@@ -66,7 +68,7 @@ public class QueueExercise {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        return queue.remove();
     }
 
     /**
@@ -76,6 +78,6 @@ public class QueueExercise {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
